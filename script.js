3927d0411c9e54b9fbe88ee0d723d544dbbf4fca
Aidan Wyber <pandakaas@gmail.com>
2025-03-17 14:56:19 +0100

    index html

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// This is a basic JavaScript script
+
+// Function to display a greeting message
+function greet() {
+    console.log("Hello, world!");
+}
+
+// Call the greet function
+greet();
